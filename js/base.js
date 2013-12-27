d26cc46699bb5aca39889dec5c69ab6f9cd83c7b
Yuan Xulei <xyuan@mozilla.com>
2013-12-27 11:20:18 +0800

    Play background music with JavaScript.

--- js/base.js
+++ js/base.js
@@ -29,6 +29,25 @@ else {
     console.log("Open Web Apps not supported");
 }
 
+// Hidden audio player
+var audio = document.querySelector("#player-audio");
+
+// "Play" button handler
+var playBtn = document.querySelector("#play");
+if (playBtn) {
+    playBtn.onclick = function() {
+        // Reset src before we set a new source to the audio element
+        audio.removeAttribute('src');
+        audio.load();
+        // Add mozAudioChannelType to the player to allow music playing in the
+        // background.
+        audio.mozAudioChannelType = "content";
+        audio.src = "music.ogg";
+        audio.load();
+        audio.play();
+    }
+}
+
 // Reload content
 var reload = document.querySelector("#reload");
 if (reload) {
